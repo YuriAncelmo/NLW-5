b25c75257d46a87f50dec06cf2a54db31adb9373
yuria <yuriancelmo@hotmail.com>
2021-04-21 20:51:36 -0300

    Adicionando e configurando messages e services

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,4 +1,5 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { SettingsRepository } from "../repositories/SettingsRepository";
 
 interface ISettingsCreate {
@@ -6,20 +7,24 @@ interface ISettingsCreate {
     username: string;
 }
 class SettingsService {
+    private settingsRepository: Repository<Setting>
+    constructor() {
+        this.settingsRepository = getCustomRepository(SettingsRepository);
+    }
     async create({ chat, username }: ISettingsCreate) {
 
-        const settingsRepository = getCustomRepository(SettingsRepository);
-        const userAlreadyExists = await settingsRepository.findOne({ username })
+
+        const userAlreadyExists = await this.settingsRepository.findOne({ username })
 
         if (userAlreadyExists)
             throw new Error("User already exists");
 
-        const settings = settingsRepository.create({
+        const settings = this.settingsRepository.create({
             chat,
             username
         });
 
-        await settingsRepository.save(settings);
+        await this.settingsRepository.save(settings);
     }
 
 }
